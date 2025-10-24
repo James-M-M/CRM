fae2c643d4158040dbe6ce041da83d76d11ca9ea
James McCarron <jamesmccarronuk@gmail.com>
2025-10-24 16:44:30 +0100

    added a calendar, next step is an invoice generator

--- CRM/Data/Calendar.cs
+++ CRM/Data/Calendar.cs
@@ -1,10 +1,14 @@
-﻿namespace CRM.Data
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRM.Data
 {
     public class Calendar
     {
         public Guid CalendarId { get; set; }
         public DateTime Start { get; set; }
-        public DateTime end { get; set; }
+        // Renamed to PascalCase so RadzenScheduler will find the end-time property
+        [Column("end")]
+        public DateTime End { get; set; }
         public string Text { get; set; } = string.Empty;
     }
 }
