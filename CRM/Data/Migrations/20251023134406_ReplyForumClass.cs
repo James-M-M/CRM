9217eae83785293199e7b189f88a90fac5a5603c
James McCarron <jamesmccarronuk@gmail.com>
2025-10-24 08:20:12 +0100

    added reply function to FAQ

---
+++ CRM/Data/Migrations/20251023134406_ReplyForumClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplyForumClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "replies",
+                table: "Forums");
+
+            migrationBuilder.CreateTable(
+                name: "Reply",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ForumId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reply", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reply_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reply_Forums_ForumId",
+                        column: x => x.ForumId,
+                        principalTable: "Forums",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reply_CustomerId",
+                table: "Reply",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reply_ForumId",
+                table: "Reply",
+                column: "ForumId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reply");
+
+            migrationBuilder.AddColumn<string>(
+                name: "replies",
+                table: "Forums",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- CRM/Data/Reply.cs
+++ CRM/Data/Reply.cs
@@ -1,14 +1,13 @@
 ﻿namespace CRM.Data
 {
-    public class Forum
+    public class Reply
     {
-        public Guid Id { get;set; }
-        public string Title { get; set; } = string.Empty;
+        public Guid Id { get; set; }
         public string Content { get; set; } = string.Empty;
-        
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public Guid ForumId { get; set; }
+        public Forum Forum { get; set; }
         public Guid CustomerId { get; set; }
         public Customer Customer { get; set; }
-        public ICollection<Reply> Replies { get; set; } = new List<Reply>();
     }
 }
